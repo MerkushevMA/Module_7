d804063a10613d1c060fe019b83f34b1dcb8ac4d
MerkushevMA <s2dent59@gmail.com>
2024-09-24 20:57:33 +0500

    Update Module_7_5_directory.py

--- Module_7_5_directory.py
+++ Module_7_5_directory.py
@@ -1,14 +1,14 @@
-import os
-import time
+mport time
 
-directory = '.'
+directory = 'C:\PythonJobs\Module_7'
 
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(file)
-        filetime = os.path.getmtime(file)
+        filepath = os.path.join(root)
+        filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime('%d.%m.%Y %H:%M', time.localtime(filetime))
-        filesize = os.path.getsize(file)
-        parent_dir = os.path.dirname(file)
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
         print(f'Обнаружен файл: {file}, путь: {filepath}, размер: {filesize} байт, время размещения: {formatted_time}, '
               f'родительская директория: {parent_dir}')
+    break
